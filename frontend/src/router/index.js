6ee835676e2ff3edb6abad507379c6d093db2976
Aump_PCHY <johndoe@example.com>
2026-06-16 17:26:31 +0700

    fix Registration feature

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -55,7 +55,7 @@ const router = createRouter({
     {
   path: '/signup',
   name: 'signup',
-  component: () => import('../views/SignUpView.vue'),
+  component: () => import('../views/SignupView.vue'),
   meta: { public: true }
 },
   ],
